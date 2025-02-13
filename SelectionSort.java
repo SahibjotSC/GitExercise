91e67cbb6d55e61af8f0f9607f83844e56cb8e4a
Manjindergit <manjindergrewal1997@gmail.com>
2025-02-12 21:57:47 -0800

    Updated Main and added Test case for selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -12,6 +12,7 @@ public class SelectionSort extends Sort
     {
         for (int i = 0; i < array.length - 1; i++)
         {
+            // Find the minimum element in the unsorted part of the array
             int minIndex = i;
             for (int j = i + 1; j < array.length; j++)
             {
